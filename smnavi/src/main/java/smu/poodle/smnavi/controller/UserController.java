6856385f3054729d36ae768bc1359a3b14473b42
guitarcode <choicco89@gmail.com>
2022-12-27 18:35:29 +0900

    create project

--- smnavi/src/main/java/smu/poodle/smnavi/controller/UserController.java
+++ smnavi/src/main/java/smu/poodle/smnavi/controller/UserController.java
@@ -2,6 +2,7 @@ package smu.poodle.smnavi.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import smu.poodle.smnavi.domain.User;
@@ -14,7 +15,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/api/user")
-    public User join(User user){
+    public User join(@RequestBody User user){
         userService.join(user);
         return user;
     }
