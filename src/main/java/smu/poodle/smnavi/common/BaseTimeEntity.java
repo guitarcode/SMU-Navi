c45a9d3155409dfd89384948bd79a8be5a9c336c
seminchoi <choicco89@gmail.com>
2023-08-09 13:55:32 +0900

    fix : timeStamp 미적용 오류 해결중

--- src/main/java/smu/poodle/smnavi/common/BaseTimeEntity.java
+++ src/main/java/smu/poodle/smnavi/common/BaseTimeEntity.java
@@ -1,5 +1,6 @@
 package smu.poodle.smnavi.common;
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
@@ -7,10 +8,12 @@ import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public abstract class BaseTimeEntity {
     @CreationTimestamp
