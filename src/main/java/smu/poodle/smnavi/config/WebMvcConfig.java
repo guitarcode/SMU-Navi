4ecab307806a1f6b60d5b2c571dc17298e065756
seminchoi <choicco89@gmail.com>
2023-08-13 17:51:50 +0900

    infra : 프론트 배포서버 url cors 허용

--- src/main/java/smu/poodle/smnavi/config/WebMvcConfig.java
+++ src/main/java/smu/poodle/smnavi/config/WebMvcConfig.java
@@ -9,7 +9,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://main.d3pkk4juq39oze.amplifyapp.com")
                 .allowedMethods("GET","POST","PUT","DELETE");
     }
 }
